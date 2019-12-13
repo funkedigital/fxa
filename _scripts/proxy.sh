49a5f140aed6533bb70db8edab2da7bfbfa87dcf
werkon <werner.koenig.jr@gmai.com>
2019-12-13 13:54:22 +0100

    FDL-604

--- _scripts/proxy.sh
+++ _scripts/proxy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -ex
 
-docker run --rm --name proxy -v ~/fxa/_scripts/configs/nginx.conf:/etc/nginx/nginx.conf:ro -p 80:80 nginx
+docker run --network="host" --rm --name proxy -v ~/fxa/_scripts/configs/nginx.conf:/etc/nginx/nginx.conf:ro -p 80:80 nginx
