02eab50fedb24241c6ec5997d390a0a7f6c839ff
werkon <werner.koenig.jr@gmai.com>
2020-01-09 16:44:13 +0100

    ssl

--- _scripts/proxy.sh
+++ _scripts/proxy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -ex
 
-docker run --network="host" --rm --name proxy -v ~/fxa/_scripts/configs/nginx.conf:/etc/nginx/nginx.conf:ro -p 80:80 -p 443:443 nginx
+docker run --network="host" --rm --name proxy -v ~/fxa/_scripts/configs/nginx.conf:/etc/nginx/nginx.conf:ro -v ~/fxa/_scripts/configs/nginx-self-signed.crt:/etc/nginx/nginx-self-signed.crt:ro -v ~/fxa/_scripts/configs/nginx-self-signed.key:/etc/nginx/nginx-self-signed.key:ro -p 80:80 -p 443:443 nginx
